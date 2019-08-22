c9f462f8ccf823865b3c9af256a629f5dd6b9a2a
Cristobal <cristobal.aguirre89@gmail.com>
2019-08-22 10:17:41 -0700

    finally cors is working properly

--- service.py
+++ service.py
@@ -105,8 +105,9 @@ def handler(event, context):
         return {
             'statusCode': 200,
             "headers": {
-                "Access-Control-Allow-Credentials": "true",
                 "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Allow-Methods": "'OPTIONS,POST'",
                 "Content-Type": "application/json",
             },
             'body': json.dumps({'message': 'Allowed'})
